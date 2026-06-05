1d192c5c39642e329c45cc144c7567d3acb77002
stranske <stranske@gmail.com>
2026-06-05 17:23:55 -0500

    Issue #1318: add deterministic daily menu
    Closes #1318

--- trip_planner/itinerary/__init__.py
+++ trip_planner/itinerary/__init__.py
@@ -1,6 +1,15 @@
 """Itinerary-focused derivation utilities."""
 
 from .feasibility import evaluate_bundle_feasibility
+from .daily_menu import (
+    DailyMenu,
+    MenuRollup,
+    MenuStop,
+    SourceFeedbackBandit,
+    SourceMix,
+    build_daily_menu,
+    calibrate,
+)
 from .move_costs import MoveCostSummary, TravelTimeEstimate, build_move_cost_summaries
 from .objective_derivation import derive_itinerary_objectives
 from .scenarios import (
@@ -14,7 +23,12 @@ from .scenarios import (
 from .search import assemble_itinerary_scenarios
 
 __all__ = [
+    "DailyMenu",
+    "MenuRollup",
+    "MenuStop",
     "MoveCostSummary",
+    "SourceFeedbackBandit",
+    "SourceMix",
     "TravelTimeEstimate",
     "SCENARIO_KINDS",
     "TRADEOFF_SEVERITIES",
@@ -24,6 +38,8 @@ __all__ = [
     "ScenarioTradeoff",
     "assemble_itinerary_scenarios",
     "build_move_cost_summaries",
+    "build_daily_menu",
+    "calibrate",
     "derive_itinerary_objectives",
     "evaluate_bundle_feasibility",
 ]
